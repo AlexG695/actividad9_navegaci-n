6e01e372ff0b1e0720237439624ca1857e6366d3
AlexG695 <alexg695845@gmail.com>
2022-03-18 18:00:37 -0700

    Subida del proyecto a Github

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,57 @@
-import React from "react";
-import { View, Text, Button, SafeAreaView } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { View, Text, Button, SafeAreaView, FlatList } from "react-native";
 
 export default function HomeScreen({navigation }) {
+  
+  
+  const[datos, setDatos] = useState([]);
+  
+  const getPosts = async() => {
+
+    try{
+
+      const url = "https://jsonplaceholder.typicode.com/posts";
+
+      const response = await fetch(url); //consumoir los datos
+
+      const json = await response.json(); //convertir a json
+
+      setDatos(json);
+
+    } catch (error) {
+
+      console.error(error);
+
+    }
+
+  }
+
+  useEffect(()=>{
+
+    getPosts();
+
+  }, [])
   return (
     <SafeAreaView>
-      <Text>Estamos en HomeScreen</Text>
+      
       <Button onPress={()=>{
+        <FlatList
 
+        data={datos}
+
+        keyExtractor = { ({ id }, index) => id }
+
+        renderItem = {
+
+          ({item}) => (
+
+            <Text>{item.title}</Text>
+
+          )
+
+        }
+
+        />
          navigation.navigate('Detail', {
             itemId: 8,
           });
